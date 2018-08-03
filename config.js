937f393c885448f3bcd57331fcb6ac9788dff4c7
Ganesh Radhakrishnan <ganrad01@gmail.com>
2018-08-03 11:27:40 -0700

    Added Helm Chart for k8s deployment

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let config = {}
 
-config.host = process.env.HOST || "Substitute URI value here";
-config.authKey = process.env.AUTH_KEY || "Substitute PRIMARY KEY value here";
+config.host = process.env.HOST || "https://vcs-cosmosdb.documents.azure.com:443/";
+config.authKey = process.env.AUTH_KEY || "1pxILrCIRPhunE5p9R6tA4Y5ZrqJGyNiCKD3LBh3zUSAADMuoaMa0AtdHVujR8ysWDAGTsyRtwqibLoSlnwt0A==";
 config.databaseId = "OrderDB";
 config.collectionId = "PurchaseOrders";
 
