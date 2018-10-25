26f36e6b7f530bbaf050718124cb1580600cd9c6
Ganesh Radhakrishnan <ganrad01@gmail.com>
2018-10-25 12:05:45 -0700

    Updating config for demo

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 let config = {}
 
+// Configure the host and authkey before deploying this app!!
 config.host = process.env.HOST || "https://vcs-cosmosdb.documents.azure.com:443/";
 config.authKey = process.env.AUTH_KEY || "1pxILrCIRPhunE5p9R6tA4Y5ZrqJGyNiCKD3LBh3zUSAADMuoaMa0AtdHVujR8ysWDAGTsyRtwqibLoSlnwt0A==";
 config.databaseId = "OrderDB";
